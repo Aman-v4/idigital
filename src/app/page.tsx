ea6fd81de57b65741d4ca2702d268fb63b2f5cb1
Aman <namaverma435@gmail.com>
2025-08-26 21:29:29 +0530

    first top section 50% made

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,6 @@
 "use client"
 import { SidebarProvider, Sidebar, SidebarBody, SidebarLink } from "@/components/ui/sidebar";
-import { House,AlignEndHorizontal,ShoppingCart,User,ChartSpline,MessageCircle,Settings,Star,Award} from "lucide-react";
+import { House,AlignEndHorizontal,ShoppingCart,User,ChartSpline,MessageCircle,Settings,Star,Award,BellDot,ChevronDown} from "lucide-react";
 import { useState } from "react";
 
 export default function DashboardLayout({
@@ -70,7 +70,39 @@ export default function DashboardLayout({
           </SidebarBody>
         </Sidebar>
         <main className="flex-1 overflow-auto">
-          {children}
+          <div className="flex flex-col gap-4 mt-16 mx-10">
+            <div className="border-2 flex justify-between items-center border-red-500 h-[100px]  rounded-lg">
+                <div>
+                  <p className="text-3xl tracking-wide font-medium">Hello, <span className=" text-[#A9DFD8] ">  Aman Verma🤘</span></p>
+                </div>
+
+                <div className="flex items-center gap-6">
+                  <BellDot />
+                  <div className="flex items-center gap-2">
+                    <div className="rounded-full bg-[#A9DFD8] w-10 h-10">
+                    </div>
+                    <div>
+                      <ChevronDown />
+                    </div>
+
+                  </div>
+                </div>
+
+
+            </div>
+
+            <div className="border-2 border-red-500 h-[100px] rounded-lg">
+
+            </div>
+
+            <div>
+
+            </div>
+
+            <div>
+
+            </div>
+          </div>
         </main>
       </div>
     </SidebarProvider>
