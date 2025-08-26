c26e5fb0d8dbb54c57fc9c0a00ec681857b710a9
Aman <namaverma435@gmail.com>
2025-08-26 22:15:43 +0530

    second section done 80%

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -70,10 +70,10 @@ export default function DashboardLayout({
           </SidebarBody>
         </Sidebar>
         <main className="flex-1 overflow-auto">
-          <div className="flex flex-col gap-4 mt-16 mx-10">
+          <div className="flex flex-col gap-5 mt-14 mx-10">
 
 
-            <div className="border-2 flex justify-between items-center border-red-500 h-[100px]  rounded-lg">
+            <div className="flex justify-between items-center h-[60px]  rounded-lg">
                 <div>
                   <p className="text-3xl tracking-wide font-medium">Hello, <span className=" text-[#A9DFD8] ">  Aman Verma🤘</span></p>
                 </div>
@@ -91,13 +91,42 @@ export default function DashboardLayout({
                 </div>
             </div>
 
-            <div className="gradient-border border-b-0.5 border-[#a9dfd891] relative h-[100px] bg-[#21222D] rounded-lg">
 
+
+            <div className="flex gap-5 items-center h-[60px] bg-[#21222D] rounded-lg px-6 shadow-lg  ">
+              <div className="border-r-1 pr-5 h-full items-center flex border-gray-600/50 ">
+                <p className="text-3xl font-semibold text-gray-300 tracking-wider">
+                  6<span className="text-sm text-gray-400/50">Days </span>
+                  11<span className="text-sm text-gray-400/50">Hr </span>
+                  59<span className="text-sm text-gray-400/50">Min </span>
+                  09<span className="text-sm text-gray-400/50">Sec </span>  
+                </p>
+              </div>
+              <div>
+                <p className="text-gray-300 tracking-wider text-sm">
+                  Your <span className="text-[#A9DFD8]">next</span> level is just <span className="text-[#A9DFD8]">one click away...</span> 
+                </p>
+              </div>
             </div>
 
-            <div>
 
+
+            <div className="flex gap-5 ">
+              <div className=" flex-[7] gradient-border border-b-0.5 border-[#a9dfd891] relative h-[300px] rounded-lg bg-[#21222D] shadow-lg py-6 px-4">
+                <div className="flex flex-col gap-2">
+                  <p className="text-gray-300 text-xl font-semibold tracking-wider">Earning Insights</p>
+                  <p className="text-gray-400 text-sm  tracking-wider">Performance Summary</p>
+                </div>
+                
+                <div></div>
+              </div>
+
+              <div className=" flex-[3] gradient-border border-b-0.5 border-[#a9dfd891] relative h-[300px] rounded-lg bg-[#21222D] shadow-lg py-6 px-4">
+
+              </div>
             </div>
+
+
 
             <div>
 
